59bf760175bb8cb5a4420ce1a936e9fdb3a6d1f1
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-06-21 23:48:30 +0530

    socket connect

--- socketapi.js
+++ socketapi.js
@@ -6,6 +6,16 @@ const socketapi = {
 // Add your socket.io logic here!
 io.on( "connection", function( socket ) {
     console.log( "A user connected" );
+
+    socket.on('join', async username => {
+        await userModel.findOneAndUpdate({
+            username
+        }, {
+            socketId: socket.id
+            
+        })
+    })
+
 });
 // end of socket.io logic
 
